aa8506aaf4791afc66a1d9520af59d18dcf33fdc
ishwar6 <iisudrj11@gmail.com>
2021-11-21 14:15:33 +0530

    Base

---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const Todos = () => {
+let todos = [
+    {
+        sno: 1,
+        title: "Read Newspaper",
+        desc: "Read the newspaper quickly"
+    },
+    {
+        sno: 2,
+        title: "Read History",
+        desc: "Read the Modern History quickly"
+    },
+    {
+        sno: 3,
+        title: "Read Economics",
+        desc: "Read the Eco quickly"
+    }
+]
+
+    return (
+        <div>
+            Todos works
+        </div>
+    )
+}
